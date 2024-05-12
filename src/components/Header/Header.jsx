92ec22391e337a7e3aa696af4a1482d10ca4fc2d
Souleymane7800 <souleymane.doumbia7800@gmail.com>
2024-05-12 12:55:15 +0200

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Logo from '../../assets/logo.png';
+import '../../styles/header.scss';
+import Navbar from '../Navbar/Navbar';
+
+export default function Header() {
+      return (
+            <div className='header'>
+                  <div>
+                        <img className='logo' src={Logo} alt='logo kasa' />
+                  </div>
+                  <Navbar />
+            </div>
+      );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import '../../styles/navbar.scss';
+
+export default function Navbar() {
+      return (
+            <nav className='navbar'>
+                  <ul>
+                        <li>Accueil</li>
+                        <li>A Propos</li>
+                  </ul>
+            </nav>
+      );
+}
