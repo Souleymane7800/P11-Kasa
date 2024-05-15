0d57bd1f0e5518927c325849fc1f07500d95003f
Souleymane7800 <souleymane.doumbia7800@gmail.com>
2024-05-15 11:32:53 +0200

    collapse

---
+++ src/components/SlideShow/SlideShow.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import '../../styles/components/slideShow.scss';
+import aboutBanner from '../../assets/background.jpg'
+
+export default function SlideShow() {
+      return <div>{aboutBanner}</div>;
+}
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,30 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import '../../styles/pages/about.scss';
+import aboutData from '../../data/aboutData.json';
+import Collapse from '../../components/Collapse/Collapse';
 
 export default function About() {
-      return <div>About</div>;
+      return (
+            <>
+                  <div className='about__img'>
+                        {/* <SlideShow /> */}
+                  </div>
+                  <div className='about__container'>
+                        {aboutData.map((data) => {
+                              return (
+                                    <div
+                                          key={data.id}
+                                          className='collapse__panel'
+                                    >
+                                          <Collapse
+                                                title={data.title}
+                                                content={data.content}
+                                          />
+                                    </div>
+                              );
+                        })}
+                  </div>
+            </>
+      );
 }
